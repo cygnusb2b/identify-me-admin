f6802c312b87a6349b4579f1acc62fa52deaab30
Jacob Bare <jacob@limit0.io>
2017-06-12 11:28:59 -0500

    Update embed code to include props

--- app/components/campaign/email-signup/embed-code.js
+++ app/components/campaign/email-signup/embed-code.js
@@ -1,12 +1,29 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component, computed, get, String: { dasherize } } = Ember;
 
 export default Component.extend({
-  campaignId: null,
-  isNew: false,
+  campaign: null,
 
-  embedCode: computed('campaignId', function() {
-    return `<div class="id-me" data-component="EmailSignup" data-prop-id="${this.get('campaignId')}"></div>`;
+  isNew: computed('campaign.isNew', function() {
+    return this.get('campaign.isNew');
+  }),
+
+  props: computed('campaign.id', 'campaign.callToAction', 'campaign.description', 'campaign.buttonValue', 'campaign.previewUrl', function() {
+    const keys = ['id', 'callToAction', 'description', 'buttonValue', 'previewUrl'];
+    const props = [];
+    const campaign = this.get('campaign');
+    keys.forEach(key => {
+      const value = get(campaign, key);
+      if (value) {
+        const name = `data-prop-${dasherize(key)}`;
+        props.pushObject(`${name}="${value}"`);
+      }
+    });
+    return props;
+  }),
+
+  embedCode: computed('props.[]', function() {
+    return `<div class="id-me" data-component="EmailSignupCampaign" ${this.get('props').join(' ')}></div>`;
   }),
 });

18ae9d8bcfa31100a52e38d0c0d2ec0f516a6796
Jacob Bare <jacob@limit0.io>
2017-06-20 10:28:38 -0500

    Add more descriptive edit headers

--- app/models/campaign.js
+++ app/models/campaign.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import SoftDeleteable from 'admin/models/mixins/soft-deleteable';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import { fragmentArray } from 'ember-data-model-fragments/attributes';
+
+const { computed } = Ember;
 
 export default Model.extend(SoftDeleteable, {
   name: attr('string'),
@@ -12,4 +15,11 @@ export default Model.extend(SoftDeleteable, {
   cookies: fragmentArray('identification-cookie'),
   setCookies: fragmentArray('identification-cookie'),
   forms: fragmentArray('campaign-form'),
+  type: computed('typeKey', function() {
+    const parts = this.get('typeKey').split('-');
+    return parts.map((part) => Ember.String.capitalize(part)).join(' ');
+  }),
+  typeKey: computed(function() {
+    return this.get('constructor.modelName').replace('campaign-', '');
+  }),
 });
